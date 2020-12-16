6b98a709f6fa8a1abc26f90d63739bcb72f7d7db
Allahpayı Gülmalıyev <allahpayi66@gmail.com>
2020-12-16 10:04:23 +0400

    15.12.2020-END

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// Add active class to the current menu item
+// var menuItems = document.getElementsByClassName("menu-link");
+// for (var i = 0; i < menuItems.length; i++) {
+//     menuItems[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
