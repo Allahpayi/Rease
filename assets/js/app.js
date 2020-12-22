c81f5eb03ffe70d87b3d7c3f71c9c567782e5821
Allahpayı Gülmalıyev <allahpayi66@gmail.com>
2020-12-22 21:28:41 +0400

    ABOUT-US-COMPLETED

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,34 @@ $(document).ready(function () {
             once: true
         });
     }
+    // Slider  Show
+    let slideIndex = 0;
+    function slide() {
+        let slideItem = document.getElementsByClassName("slide-item");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slideItem.length; i++) {
+            slideItem[i].style.display = "none";
+        }
+        slideIndex++
+        if (slideIndex > slideItem.length) {
+            slideIndex = 1
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active-dot", "");
+        }
+        slideItem[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active-dot";
+        setTimeout(slide, 2000)
+
+    }
+
     $(window).on('load', function () {
         aos_init();
+        slide()
+
     });
 
 });
+
+
+
